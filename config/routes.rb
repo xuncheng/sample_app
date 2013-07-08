b3e0204a1995ae93be71740fd68c6c7e684417ac
Xuncheng Wang <w.xun.cheng@gmail.com>
2013-07-08 12:00:21 +0800

    add user signup route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ SampleApp::Application.routes.draw do
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  get '/signup', to: 'users#new'
 end
